da9cf1f6ed13898a7c6575528d58bc8afa68c6eb
Kakao200 <kakao200@example.com>
2025-12-01 19:13:21 +0300

    Добавлены новые призы (v22.1)

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from aiogram.filters import Command
 from aiogram.types import WebAppInfo, ReplyKeyboardMarkup, KeyboardButton
 
 TOKEN = "8357449113:AAHF_heoj3besQ4YhGeuysHHNDeZjRFVHYc"
-WEBAPP_URL = "https://Kakao200.github.io/miniapp/v=10"
+WEBAPP_URL = "https://Kakao200.github.io/miniapp/?v=20251201_5"
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
